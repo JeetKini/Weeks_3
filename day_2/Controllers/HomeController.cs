ff3bedadc9a8176d6d57fe0cf76ab78a338f9dbf
JeetKini <151983803+JeetKini@users.noreply.github.com>
2023-12-12 19:28:06 +0530

    Add files via upload

--- day_2/Controllers/HomeController.cs
+++ day_2/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication13.Models;
 
-namespace WebApplication8.Controllers
+namespace WebApplication13.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.MyCustomName = "Jeet";
+            ViewBag.MyList=new List<string>() {"jeet","adnan" };
+
+            List<Employee> employees = new List<Employee>() {
+            new Employee(){Email= "Jeet@gmail.com",Id=1,Name="jeet"},
+            new Employee(){Email= "Meet@gmail.com",Id=2,Name="Meet"},
+            new Employee(){Email= "Geet@gmail.com",Id=3,Name="Geet"}
+            };
+            ViewBag.MyEmpList=employees;
+
+            return View();
+        }
+        public ActionResult Demo()
+        {
+            ViewData["myKey"]="This is My ValueData";
+            ViewData["MyList"] = new List<string>() { "jeet", "adnan" };
             return View();
         }
     }
---
+++ day_2/Views/CustomHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication12.Views
+{
+    public static class CustomHelper
+    {
+        public static IHtmlString Image(string src,string alt)
+        {
+            return new MvcHtmlString(string.Format("<img Src='{0}' Alt='{1}'></img>",src,alt));
+        }
+        public static IHtmlString Img(string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img Src='{0}' Alt='{1}'></img>", src, alt));
+        }
+        public static IHtmlString ImgTag(string src, string alt)
+        {
+            TagBuilder tag=new TagBuilder("img");
+            tag.Attributes.Add("src", src);
+            tag.Attributes.Add("alt", alt);
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
